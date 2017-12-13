95ecfebd053d150b21f70c2ca60cd93a227a6250
itzou air <364778792@qq.com>
2017-12-13 18:33:58 +0800

    区分本地和线上环境下的路由模式

--- src/router/index/index.js
+++ src/router/index/index.js
@@ -2,9 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 Vue.use(Router)
-
 export default new Router({
-  mode:'history',
+  mode:process.env.NODE_ENV === 'production' ?'history':'hash',
   routes: [
     {
       path: '/',
